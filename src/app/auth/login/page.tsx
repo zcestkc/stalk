96a467a8ab0ff50ce5b4cd71c2512826fe8578f7
kwanchan <kwan9545@gmail.com>
2025-01-16 20:25:07 +0000

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import { paths } from '@/config/paths';
+import { LoginForm } from '@/features/auth/components/login-form';
+
+const LoginPage = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const redirectTo = searchParams?.get('redirectTo');
+
+  return (
+    // <LoginForm
+    //   onSuccess={() =>
+    //     router.replace(
+    //       `${redirectTo ? `${decodeURIComponent(redirectTo)}` : paths.app.dashboard.getHref()}`,
+    //     )
+    //   }
+    // />
+    <h1>Login</h1>
+  );
+};
+
+export default LoginPage;
