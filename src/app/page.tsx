eca17648f41f7ebb130435433f686f5db3a8ac92
kwanchan <kwan9545@gmail.com>
2025-02-13 19:39:07 +0000

    remove unneeded svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <div className="flex h-screen items-center bg-white">
       <div className="mx-auto max-w-7xl px-4 py-12 text-center sm:px-6 lg:px-8 lg:py-16">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl select-none">
           Stalk
         </h2>
         <Typewriter text="An investment simulator" delay={100} infinite />
