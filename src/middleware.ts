8df0f3d4145796c70c448087be24423c43209452
kwanchan <kwan9545@gmail.com>
2025-02-19 00:41:04 +0000

    refine middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 import { NextResponse, type NextRequest } from 'next/server';
 
 // Middleware to handle token refresh
@@ -13,55 +12,39 @@ export async function middleware(request: NextRequest) {
   const refreshToken = request.cookies.get('refreshToken');
 
   if (pathname === '/auth/login') {
-    // console.log('Login page accessed, skipping middleware.');
-    return NextResponse.next();
+    return NextResponse.next(); // Login page accessed, skipping middleware.
   }
 
   if (!refreshToken) {
-    // console.log('No refresh token found, redirecting to login.');
-    return NextResponse.redirect(new URL('/auth/login', request.url));
+    return NextResponse.redirect(new URL('/auth/login', request.url)); //No refresh token found, redirecting to login.
   }
 
   if (!accessToken) {
-    const newToken = await refreshAccessToken(refreshToken);
+    const headers = new Headers({
+      Cookie: `${refreshToken.name}=${refreshToken.value}; `,
+    });
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh-token`,
+        {
+          method: 'POST',
+          headers,
+        },
+      );
 
-    if (newToken) {
-      //   console.log('Token refreshed successfully!');
+      if (response.ok) {
+        const cookies = response.headers.get('set-cookie');
+
+        if (cookies) {
+          const nextResponse = NextResponse.next();
+          nextResponse.headers.set('Set-Cookie', cookies);
+          return nextResponse;
+        }
+      }
+    } catch (e) {
       return NextResponse.next();
-    } else {
-      //   console.log('Token refresh failed, redirecting to login.');
-      return NextResponse.redirect(new URL('/auth/login', request.url));
     }
   }
 
   return NextResponse.next();
-}
-
-async function refreshAccessToken(refreshToken: RequestCookie) {
-  const headers = new Headers({
-    Cookie: `${refreshToken.name}=${refreshToken.value}; `,
-  });
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh-token`,
-      {
-        method: 'POST',
-        headers,
-      },
-    );
-
-    if (response.ok) {
-      const cookies = response.headers.get('set-cookie');
-      if (cookies) {
-        const nextResponse = NextResponse.next();
-        nextResponse.headers.set('Set-Cookie', cookies);
-        return nextResponse;
-      }
-      return NextResponse.next();
-    }
-
-    return null;
-  } catch (error) {
-    return null;
-  }
 }
