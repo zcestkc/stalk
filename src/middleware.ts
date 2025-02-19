fb1f3e790ab4bde0f2c070e20c810fc05a37c3da
kwanchan <kwan9545@gmail.com>
2025-02-19 01:08:44 +0000

    config matcher for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,11 @@ import { NextResponse, type NextRequest } from 'next/server';
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  if (!pathname.startsWith('/app') && !pathname.startsWith('/auth')) {
-    return NextResponse.next();
-  }
-
   const accessToken = request.cookies.get('accessToken');
   const refreshToken = request.cookies.get('refreshToken');
 
-  if (pathname === '/auth/login') {
-    return NextResponse.next(); // Login page accessed, skipping middleware.
+  if (pathname === '/auth/login' || pathname === '/') {
+    return NextResponse.next(); // Login page or landing page accessed, skipping middleware.
   }
 
   if (!refreshToken) {
@@ -49,3 +45,16 @@ export async function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, logo.svg,  sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|logo.svg|sitemap.xml|robots.txt).*)',
+  ],
+};
